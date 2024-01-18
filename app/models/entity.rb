297418d8f2a05daa9b48cac760ab14f0b961f824
Peter Akhigbe <akhigbe37@gmail.com>
2024-01-18 10:51:14 +0100

    Add validation in models

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,5 +1,8 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :group_entities
-  has_many :groups, through: :group_entities
+  has_many :group_entities, dependent: :destroy
+  has_many :groups, through: :group_entities, dependent: :destroy
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups, dependent: :destroy
+  has_many :entities, foreign_key: :author_id, dependent: :destroy
+
+  validates :name, presence: true, length: { within: 4..250 }
+  validates :email, presence: true
+  validates :password, presence: true
 end
